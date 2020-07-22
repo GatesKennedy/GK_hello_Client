341134ccdc21b424e18cc3e8d58a203f9aa800ce
GatesKennedy <conor@esccollective.com>
2020-07-22 01:02:00 -0700

    MOD: TS Heroku - xStrictMode, hide env, set env

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 console.log('REACT_APP_NAME= ', process.env.REACT_APP_NAME);
 console.log('REACT_APP_API_URL= ', process.env.REACT_APP_API_URL);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
